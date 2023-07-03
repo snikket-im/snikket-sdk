e347504a91f3b5e5dc8e39509291cf6b4cc9c2ce
Stephen Paul Weber <singpolyma@singpolyma.net>
2023-07-03 13:29:39 -0500

    Use a proper JID class
    So it can have methods, etc

--- xmpp/JID.hx
+++ xmpp/JID.hx
@@ -1,22 +1,46 @@
 package xmpp;
 
-typedef SplitJID = {
-	var ?node : String;
-	var domain : String;
-	var ?resource : String;
-};
-
 class JID {
-	public static function split(jid:String):SplitJID {
+	public final node : Null<String>;
+	public final domain : String;
+	public final resource : Null<String>;
+
+	public function new(?node:String, domain:String, ?resource:String) {
+		this.node = node;
+		this.domain = domain;
+		this.resource = resource;
+	}
+
+	public static function parse(jid:String):JID {
 		var resourceDelimiter = jid.indexOf("/");
 		var nodeDelimiter = jid.indexOf("@");
-		if(nodeDelimiter >= resourceDelimiter) {
+		if(resourceDelimiter > 0 && nodeDelimiter >= resourceDelimiter) {
 			nodeDelimiter = -1;
 		}
-		return {
-			node: (nodeDelimiter>0)?jid.substr(0, nodeDelimiter):null,
-			domain: jid.substring((nodeDelimiter == -1)?0:nodeDelimiter+1, (resourceDelimiter == -1)?jid.length+1:resourceDelimiter),
-			resource: (resourceDelimiter == -1)?null:jid.substring(resourceDelimiter+1),
-		};
+		return new JID(
+			(nodeDelimiter>0)?jid.substr(0, nodeDelimiter):null,
+			jid.substring((nodeDelimiter == -1)?0:nodeDelimiter+1, (resourceDelimiter == -1)?jid.length+1:resourceDelimiter),
+			(resourceDelimiter == -1)?null:jid.substring(resourceDelimiter+1)
+		);
+	}
+
+	public function asBare():JID {
+		return new JID(this.node, this.domain);
+	}
+
+	public function equals(rhs:JID):Bool {
+		return (
+			this.node == rhs.node &&
+			this.domain == rhs.domain &&
+			this.resource == rhs.resource
+		);
+	}
+
+	public function asString():String {
+		return (
+			(this.node != null ? this.node + "@" : "") +
+			this.domain +
+			(this.resource != null ? "/" + this.resource : "")
+		);
 	}
 }
