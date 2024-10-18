454c84feb611534447dd649b73a915bd4c652c47
Stephen Paul Weber <singpolyma@singpolyma.net>
2024-10-17 21:47:31 -0500

    Commit compiled code to a branch

---
+++ npm/index.d.ts
@@ -0,0 +1,28 @@
+import browserp from "./browser.js";
+import { snikket as enums } from "./snikket-enums.js";
+import { snikket } from "./snikket.js";
+export import AvailableChat = snikket.AvailableChat;
+export import Caps = snikket.Caps;
+export import Channel = snikket.Channel;
+export import Chat = snikket.Chat;
+export import ChatAttachment = snikket.ChatAttachment;
+export import ChatMessage = snikket.ChatMessage;
+export import Client = snikket.Client;
+export import Config = snikket.Config;
+export import DirectChat = snikket.DirectChat;
+export import Hash = snikket.Hash;
+export import Identicon = snikket.Identicon;
+export import Identity = snikket.Identity;
+export import Notification = snikket.Notification;
+export import Participant = snikket.Participant;
+export import Push = snikket.Push;
+export import SerializedChat = snikket.SerializedChat;
+export import jingle = snikket.jingle;
+export import UiState = enums.UiState;
+export import MessageStatus = enums.MessageStatus;
+export import MessageDirection = enums.MessageDirection;
+export import UserState = enums.UserState;
+export declare namespace persistence {
+    export import browser = browserp;
+    export import Dummy = snikket.persistence.Dummy;
+}
---
+++ npm/snikket-enums.d.ts
@@ -0,0 +1,31 @@
+ 
+export declare namespace snikket {
+    const enum UiState {
+        Pinned = 0,
+        Open = 1,
+        Closed = 2
+    }
+}
+export declare namespace snikket {
+    const enum MessageDirection {
+        MessageReceived = 0,
+        MessageSent = 1
+    }
+}
+export declare namespace snikket {
+    const enum MessageStatus {
+        MessagePending = 0,
+        MessageDeliveredToServer = 1,
+        MessageDeliveredToDevice = 2,
+        MessageFailedToSend = 3
+    }
+}
+export declare namespace snikket {
+    const enum UserState {
+        Gone = 0,
+        Inactive = 1,
+        Active = 2,
+        Composing = 3,
+        Paused = 4
+    }
+}
