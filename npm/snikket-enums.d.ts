7ed0766945328987f118114767c4cc673096f821
Stephen Paul Weber <singpolyma@singpolyma.net>
2024-11-07 22:11:10 -0500

    Commit compiled code to a branch

---
+++ npm/snikket-enums.d.ts
@@ -0,0 +1,39 @@
+ 
+export declare namespace snikket {
+    const enum UiState {
+        Pinned = 0,
+        Open = 1,
+        Closed = 2
+    }
+}
+export declare namespace snikket {
+    const enum MessageType {
+        MessageChat = 0,
+        MessageCall = 1,
+        MessageChannel = 2,
+        MessageChannelPrivate = 3
+    }
+}
+export declare namespace snikket {
+    const enum MessageDirection {
+        MessageReceived = 0,
+        MessageSent = 1
+    }
+}
+export declare namespace snikket {
+    const enum MessageStatus {
+        MessagePending = 0,
+        MessageDeliveredToServer = 1,
+        MessageDeliveredToDevice = 2,
+        MessageFailedToSend = 3
+    }
+}
+export declare namespace snikket {
+    const enum UserState {
+        Gone = 0,
+        Inactive = 1,
+        Active = 2,
+        Composing = 3,
+        Paused = 4
+    }
+}
