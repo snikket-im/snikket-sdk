7eef0397314f5c24dd77827d4493c38a5305cb8f
Stephen Paul Weber <singpolyma@singpolyma.net>
2024-12-09 15:25:21 -0500

    Commit compiled code to a branch

---
+++ npm/tmp99260593.js
@@ -0,0 +1,8 @@
+import snikket_streams_XmppJsScramSha1 from "@xmpp/sasl-scram-sha-1";
+import { client as snikket_streams_XmppJsClient } from "@xmpp/client";
+import { jid as snikket_streams_XmppJsJID } from "@xmpp/jid";
+import snikket_streams_XmppJsDebug from "@xmpp/debug";
+import snikket_streams_XmppJsXml from "@xmpp/xml";
+import * as snikket_streams_XmppJsLtx from "ltx";
+import snikket_streams_XmppJsId from "@xmpp/id";
+import snikket_streams_XmppJsError from "@xmpp/error";
