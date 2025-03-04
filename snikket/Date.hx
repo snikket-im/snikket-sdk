a04f08a1bcec59067e6502ced2e5631389d7b6a7
Stephen Paul Weber <singpolyma@singpolyma.net>
2025-03-04 11:20:40 -0500

    Always do UTC strings for dates
    Easier to compare

--- snikket/Date.hx
+++ snikket/Date.hx
@@ -2,12 +2,11 @@ package snikket;
 
 class Date {
 	public static function format(d: std.Date):String {
-		var str = DateTools.format(d, "%Y-%m-%dT%H:%M:%S");
-		var tzHour = Std.int(d.getTimezoneOffset()/60);
-		var tzMinute = Std.int(Math.abs(d.getTimezoneOffset())%60);
-		return
-			str + (tzHour < 0 ? "+" : "-") +
-			StringTools.lpad(Std.string(Math.abs(tzHour)), "0", 2) + ":" +
-			StringTools.lpad(Std.string(tzMinute), "0", 2);
+		return Std.string(d.getUTCFullYear()) + "-" +
+			StringTools.lpad(Std.string(d.getUTCMonth() + 1), "0", 2) + "-" +
+			StringTools.lpad(Std.string(d.getUTCDate()), "0", 2) + "T" +
+			StringTools.lpad(Std.string(d.getUTCHours()), "0", 2) + ":" +
+			StringTools.lpad(Std.string(d.getUTCMinutes()), "0", 2) + ":" +
+			StringTools.lpad(Std.string(d.getUTCSeconds()), "0", 2) + ".000Z";
 	}
 }
