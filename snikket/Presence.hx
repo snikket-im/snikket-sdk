2bd41cb73c16f3155f35333fdf765130ab3f47c3
Stephen Paul Weber <singpolyma@singpolyma.net>
2025-03-04 11:52:11 -0500

    Presence mucUser is immutable

--- snikket/Presence.hx
+++ snikket/Presence.hx
@@ -3,7 +3,7 @@ package snikket;
 @:expose
 class Presence {
 	public var caps:Null<Caps>;
-	public var mucUser:Null<Stanza>;
+	public final mucUser:Null<Stanza>;
 
 	public function new(caps: Null<Caps>, mucUser: Null<Stanza>) {
 		this.caps = caps;
