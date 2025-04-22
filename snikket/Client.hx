e102aed92419cc360945b407fb92ee608904d95a
Matthew Wild <mwild1@gmail.com>
2025-04-22 20:22:03 +0100

    OMEMO: Enable client-wide encryption policy support
    With the default policy, this fixes encryption to recipients with no published
    keys.
    
    Note that there is no API to set a custom encryption policy yet. The default
    is opportunistic.

--- snikket/Client.hx
+++ snikket/Client.hx
@@ -12,6 +12,7 @@ import snikket.ChatMessage;
 import snikket.Message;
 import snikket.EventEmitter;
 import snikket.EventHandler;
+import snikket.EncryptionPolicy;
 #if !NO_OMEMO
 import snikket.OMEMO;
 #end
@@ -96,6 +97,12 @@ class Client extends EventEmitter {
 	private var token: Null<String> = null;
 	private var fastCount: Null<Int> = null;
 	private final pendingCaps: Map<String, Array<(Null<Caps>)->Chat>> = [];
+	@:allow(snikket)
+	private final encryptionPolicy:EncryptionPolicy = {
+		allowUnencryptedOutgoing: true,
+		allowUnencryptedIncoming: true,
+		preferEncryptedOutgoing: true,
+	};
 
 #if !NO_OMEMO
 	@:allow(snikket)
