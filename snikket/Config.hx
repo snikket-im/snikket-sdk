032752be6fa70e956f4cda3a8752b7b5f103cd6e
Stephen Paul Weber <singpolyma@singpolyma.net>
2025-06-24 12:59:49 -0500

    enableConstrainedMemoryMode
    For at least iOS NSE we want to tell the GC to keep less headroom to
    stay away from that 24MB limit.

--- snikket/Config.hx
+++ snikket/Config.hx
@@ -1,6 +1,15 @@
 package snikket;
 
+#if cpp
+import HaxeCBridge;
+import cpp.NativeGc;
+#end
+
 @:expose
+#if cpp
+@:build(HaxeCBridge.expose())
+@:build(HaxeSwiftBridge.expose())
+#end
 class Config {
 	/**
 		Produce /.well-known/ni/ paths instead of ni:/// URIs
@@ -9,4 +18,18 @@ class Config {
 		This can be useful eg for intercepting with a Service Worker.
 	**/
 	public static var relativeHashUri = false;
+
+	@:allow(snikket)
+	private static var constrainedMemoryMode = false;
+
+	#if cpp
+	/**
+		Trades off some performance for lower / more consistent memory usage
+	**/
+	public static function enableConstrainedMemoryMode() {
+		NativeGc.setMinimumFreeSpace(500000);
+		NativeGc.setTargetFreeSpacePercentage(5);
+		constrainedMemoryMode = true;
+	}
+	#end
 }
