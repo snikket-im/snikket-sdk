5550910f5c9cd251eea447bba4e381cec873fc74
Stephen Paul Weber <singpolyma@singpolyma.net>
2026-06-03 22:15:14 -0500

    Sometimes CI was still timing out

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-	timeout: 120000,
+	timeout: 260000,
 	testDir: "./test",
 	/* Run tests in files in parallel */
 	fullyParallel: true,
